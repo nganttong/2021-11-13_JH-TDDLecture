ef4a6dc13c7224df1292378a66d9b857f1aab801
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-13 15:26:24 -0500

    added unsolved edge cases

--- src/test/java/TestMainApplication.java
+++ src/test/java/TestMainApplication.java
@@ -14,6 +14,10 @@ public class TestMainApplication {
         //given: an array of number as string { "1", "5", "15", "9", "4"}
         //task: sort the numbers
         //restriction: numbers must stay strings, no conversions to int
+        //null values should be removed prior to sorting
+        //methods should be able to account for null arrays
+        //empty strings should be sorted as if they were at 0
+        //
         //return an array of string numbers in proper order 1,4,5,9,15
         //Expected: {"1","4","5","9","15"}
 
@@ -35,6 +39,108 @@ public class TestMainApplication {
         //then or assert
         assertArrayEquals(expected,actual);
     }
+
+    @Test
+    public  void  testWithDecimals() {
+        String[] givenArray = {"c","1", "5", "15", "9", "4"};
+        String[] expected = {"1", "c","4","5","9","15"};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public  void  testWithLeadingZeros() {
+        String[] givenArray = {"007","1", "5", "15", "9", "4"};
+        String[] expected = {"1", "4","5","007","9","15"};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public  void  testLowerCaseCharacters() {
+        String[] givenArray = {"c","1", "5", "15", "9", "4"};
+        String[] expected = {"1","4","5","9","15"};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+ //solvable?
+//    @Test
+//    public  void  testWithNumberWord() {
+//        String[] givenArray = {"six","1", "5", "15", "9", "4"};
+//        String[] expected = {"1","4","5","six","9","15"};
+//        //when or act
+//        String[] actual = callMain.sortArray(givenArray);
+//        //then or assert
+//        assertArrayEquals(expected,actual);
+//    }
+
+    @Test
+    public  void  testWithEmptyString() {
+        String[] givenArray = {"1", "", "15", "9", "4"};
+        String[] expected = {"1","4","5","9","15"};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public  void  testWithNullList() {
+        String[] givenArray = null;
+        String[] expected = null;
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public  void  testWithNullString() {
+        String[] givenArray = {"1", null, "15", "9", "4"};
+        String[] expected = {"1","4", "9","15"};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public  void  testWithLengthOneArray() {
+        String[] givenArray = {"42"};
+        String[] expected = {"42"};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
+    //figured this out when we can get it to compile
+//    @Test
+//    public  void  testWhenItsNotString() {
+//        String[] givenArray = {42};
+//        String[] expected = {42};
+//        //when or act
+//        String[] actual = callMain.sortArray(givenArray);
+//        //then or assert
+//        assertArrayEquals(expected,actual);
+//    }
+
+    @Test
+    public  void  testLengthZeroArray() {
+        String[] givenArray = {};
+        String[] expected = {};
+        //when or act
+        String[] actual = callMain.sortArray(givenArray);
+        //then or assert
+        assertArrayEquals(expected,actual);
+    }
+
 
     //sub-method test
     @Test
